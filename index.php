a670ee33154a3cecd42c982216379ac108f57da5
Cory Bielski <cory.bielski@gmail.com>
2011-04-30 22:33:37 -0500

    refreshed images, updated index.php

--- index.php
+++ index.php
@@ -5,12 +5,30 @@
 	include_once("includes/dBug.php");
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-    <head>
-        <title>ZapFour</title>
-    </head>
-    <body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>ZapFour - Making Compulsive Shoppers</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+	<!-- 1140px Grid styles for IE -->
+	<!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" /><![endif]-->
+
+	<!-- The 1140px Grid - http://cssgrid.net/ -->
+	<link rel="stylesheet" href="css/1140.css" type="text/css" media="screen" />
+	
+	<!-- Your styles -->
+	<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
+	
+	<!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
+	<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
+</head>
+
+<body>
+<?php include("includes/header.php"); ?>
     <?php 
 	// If we have not received a token, display the link for Foursquare webauth
 	if($foursquare->GetAccessToken() == ""){ 
@@ -21,5 +39,6 @@
 	}
 	
 	?>
+	<?php include("includes/footer.php"); ?>
     </body>
 </html>
--- js/_vti_cnf/css3-mediaqueries.js
+++ js/_vti_cnf/css3-mediaqueries.js
@@ -1,4 +1,4 @@
 vti_encoding:SR|utf8-nl
 vti_timelastmodified:TR|30 Apr 2011 02:02:47 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|index.html
+vti_backlinkinfo:VX|index.php index.html
