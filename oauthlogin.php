0da578f07590988742ee9a306cc9555f012569eb
Ryan Anklam <ryan.anklam@gmail.com>
2011-04-30 23:19:16 -0500

    removed junk

--- oauthlogin.php
+++
@@ -1,3 +0,0 @@
-
-<a href="https://foursquare.com/oauth2/authenticate?client_id=YLCSLN3T0TOVU2O3PCYSL312FYGDPTPIAKGYTI0GMKLWZFP31&response_type=json&redirect_uri=http://www.zapfour.com">
-authorize</a>
