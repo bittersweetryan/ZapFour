35920fb0ddd4fff9a993027d47c58dc1bace13f5
Ryan Anklam <ryan.anklam@gmail.com>
2011-05-01 03:12:29 -0500

    added foursquaredao

--- classes/FoursquareDAO.php
+++ classes/FoursquareDAO.php
@@ -1,3 +1,34 @@
+<?php
+
 class FoursquareDAO {
 
+	public function getKeywordsForCategories($categories){
+		$sql = " SELECT 
+					* 
+				FROM 
+					Category C
+				INNER JOIN
+					JoinCategoryKeyword J ON J.categoryID = C.categoryID
+				INNER JOIN
+					Keyword K ON K.keywordID = J.keywordID
+				WHERE 
+					C.categoryName in (";
+		$sql .= turnCategoriesIntoList($categories);
+		
+		$sql .= ");";
+	}
+
+	private function turnCategoriesIntoList($categories){
+		$list = "";
+		
+		foreach($categories as $key => $category){
+			$list .= "\"" . $category . "\",";
+		}
+		
+		
+		//Coffee Shop","Zoo or Aquarium","Music Venue","Monument or Landmark")
+	}
+	
 }
+
+?>
