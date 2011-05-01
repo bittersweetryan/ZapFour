45c60276723d7c01ea59a064802578da633a1a10
Cory Bielski <cory.bielski@gmail.com>
2011-04-30 22:44:37 -0500

    added header

---
+++ includes/_vti_cnf/header.php
@@ -0,0 +1,16 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|01 May 2011 02:58:41 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|HPPC\\Cory
+vti_modifiedby:SR|HPPC\\Cory
+vti_timecreated:TR|01 May 2011 02:58:41 -0000
+vti_cacheddtm:TX|01 May 2011 02:58:41 -0000
+vti_filesize:IR|526
+vti_cachedlinkinfo:VX|H|./ H|http://www.twitter.com/zapfour S|../images/twitter.png
+vti_cachedsvcrellinks:VX|DHUS|includes/ NHHS|http://www.twitter.com/zapfour FSUS|images/twitter.png
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
+vti_backlinkinfo:VX|index.php
---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<div id="header">
+	<div class="container">
+		<div class="row">
+			<div class="fourcol">
+				<h1><a href="./">ZapFour Shopping</a></h1>
+			</div>
+			<div class="fourcol">
+					<h2>city</h2>
+			</div>
+			<div class="fourcol last">
+					<ul id="utility">
+						<li><a href="">about</a></li>
+						<li><a href="">developers</a></li>
+						<li><a href="http://www.twitter.com/zapfour"><img alt="ZapFour Twitter" src="../images/twitter.png"></a></li>
+					</ul>
+			</div>
+		</div>
+	</div>
+<!-- End Header --></div>
