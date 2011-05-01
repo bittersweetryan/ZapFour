5133399039ddcaa695c67792a90b60bb1b14168b
Ryan Anklam <ryan.anklam@gmail.com>
2011-04-30 20:34:44 -0500

    restructure

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-include_once("init.php");
+	session_start();
+	include_once("init.php");
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
