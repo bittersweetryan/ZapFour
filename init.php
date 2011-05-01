e036ec8b9d4fe7d18c49d759f859a82ec166d88e
Ryan Nanney <serotonin@gmail.com>
2011-04-30 23:57:58 -0500

    more stuff

--- init.php
+++ init.php
@@ -6,7 +6,8 @@ error_reporting(E_ALL); ini_set("display_errors", 1);
 
 //check for existance of requried modules
 if ( !function_exists('curl_init') ) die("Required CURL module not installed.  Cannot fetch weather data.");
-define("WEBPATH","/home/ryan/htdocs/z4");
+//define("WEBPATH","/var/www/ryan.lootcouncil.com/");
+define("WEBPATH","/home/ryan/htdocs/z4/");
 
 //autoload classes, make life easer!
 spl_autoload_register(function ($className) {
